25325688f4a2fc55124acbafbe86ad3241bca86a
yahyacodes <yahya.codin@gmail.com>
2023-11-10 09:52:26 +0300

    Conflict resolved by accepting incoming changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/FormComponents/Form";
 import Progress from "./components/ProgressComponents/Progress";
-import CourseList from "./components/CourseList";
+import CourseList from "./components/CourseComponents/CourseList";
 import Form from "./Components/Form";
 import Progress from "./Components/ProgressComponents/progress";
 import "./Form.css";
 import "./Components/ProgressComponents/progress.css";
-import LandingPage from "./Pages/LandingPage";
+import LandingPage from "./LandingPage/LandingPage";
 
 function App() {
   const [course, setCourse] = useState([]);
@@ -22,8 +22,8 @@ function App() {
     <div className="ui container">
       <Form />
       <Progress />
-        <CourseList course={course} />
-<LandingPage />
+      <CourseList course={course} />
+      <LandingPage />
       </div>
     </>
   );
